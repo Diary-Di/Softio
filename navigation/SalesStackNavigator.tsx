9014490c64432ee2b7453ef79f0c4e7aa341f563
Diary <nandrasanadiary28@gmail.com>
2025-11-25 11:35:16 +0300

    ValidationScreen

--- navigation/SalesStackNavigator.tsx
+++ navigation/SalesStackNavigator.tsx
@@ -2,10 +2,12 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import withFadeBar from '../components/withFadeBar';
 import NewSalesScreen from '../screens/NewSalesScreen';
 import SalesScreen from '../screens/SalesScreen';
+import CartValidationScreen from "../screens/CartValidationScreen";
 
 export type SalesStackParamList = {
   SaleList: undefined;
   NewSale: undefined;
+  CartValidation: undefined;
 };
 
 const Stack = createNativeStackNavigator<SalesStackParamList>();
@@ -13,11 +15,13 @@ const Stack = createNativeStackNavigator<SalesStackParamList>();
 export default function SalesStackNavigator() {
   const SaleList = withFadeBar(SalesScreen, 'produit');
   const NewSale = withFadeBar(NewSalesScreen, 'produit');
+    const CartValidation = withFadeBar(CartValidationScreen, 'produit');
 
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="SaleList" component={SaleList} />
       <Stack.Screen name="NewSale" component={NewSale} />
+      <Stack.Screen name="CartValidation" component={CartValidation} />
     </Stack.Navigator>
   );
 }
